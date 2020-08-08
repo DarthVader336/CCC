5b644c5fea112eb89de57afe7058f9beebaf2020
Kai Wang <35879380+KWang31@users.noreply.github.com>
2020-08-08 14:47:58 -0700

    Update S5.java

--- 2019/S5.java
+++ 2019/S5.java
@@ -1,61 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+import java.util.*;
 public class S5 {
 
     static double key=1.5;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String[] t=br.readLine().split("\\ ");
-        int N=Integer.parseInt(t[0]);
-        int K=Integer.parseInt(t[1]);
-        int[][] a=new int[N][N];
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        int N=Integer.parseInt(st.nextToken());
+        int K=Integer.parseInt(st.nextToken());
+        long[][] a=new long[N][N];
         for (int i = 0; i < N; i++) {
-            t=br.readLine().split("\\ ");
+            st=new StringTokenizer(br.readLine());
+            
             for (int j = 0; j <=i; j++) {
-               a[i][j]=Integer.parseInt(t[j]);
+               a[i][j]=Long.parseLong(st.nextToken());
             }
         }
         long sum=0;
         if(K==1){
             for(int i=0;i<N;i++){
                 for(int j=0;j<=i;j++){
-                    sum+=(long)a[i][j];
+                    sum+=a[i][j];
                 }
             }
             System.out.println(sum); return;
         }
-        int[][] max=new int[N-1][N-1];
-        //max[i][j]= max(a[i][j], a[i+1][j], a[i+1][j+1])
+        
         for(int i=0;i<N-1;i++){
             for (int j = 0; j <=i; j++) {
-                max[i][j]=Math.max(a[i][j],Math.max(a[i+1][j],a[i+1][j+1]));
+                a[i][j]=Math.max(a[i][j],Math.max(a[i+1][j],a[i+1][j+1]));
                 
             }
             
         }
-        int done=2; int min=0; int[][] max2;
+        int done=2; int min=0; 
         while(done<K){
-            min=Math.min((int)(done*key),K);
-            max2=new int[N-min+1][N-min+1];
+            min=Math.min(((done*3)/2),K);
+            
             for(int i=0;i<=N-min;i++){
                 for(int j=0;j<=i;j++){
-                    max2[i][j]=Math.max(max[i][j],
-                            Math.max(max[i+min-done][j],max[i+min-done][j+min-done]));
+                    a[i][j]=Math.max(a[i][j],
+                            Math.max(a[i+min-done][j],a[i+min-done][j+min-done]));
                 }
             }
             done=min;
-            max=new int[N-min+1][N-min+1];
-            for(int i=0;i<=N-min;i++){
-                for(int j=0;j<=i;j++){
-                    max[i][j]=max2[i][j];
-                }
-            }
+            
         }
         for (int i = 0; i <=N-K; i++) {
             for (int j = 0; j <=i; j++) {
-                sum+=(long)max[i][j];
+                sum+=a[i][j];
                 
             }
         }
